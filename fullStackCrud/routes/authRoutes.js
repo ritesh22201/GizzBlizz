2e69ef4314bc41c359b1890c998ce4f841b9d951
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-14 23:50:58 +0530

    code added

--- fullStackCrud/routes/authRoutes.js
+++ fullStackCrud/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const UserModel = require('../models/userModel');
 const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => {
@@ -7,7 +8,8 @@ userRouter.get('/', (req, res) => {
 
 app.post('/register', async(req, res) => {
     try {
-        
+       const user = await UserModel.create(req.body); 
+       res.status(200).send({'msg' : 'User registered successfully', user});
     } catch (error) {
         
     }
