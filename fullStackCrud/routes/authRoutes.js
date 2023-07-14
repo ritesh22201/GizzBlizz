3710f476ef1adef602353ac0095d814c292905f1
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-15 01:00:51 +0530

    code added

--- fullStackCrud/routes/authRoutes.js
+++ fullStackCrud/routes/authRoutes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const UserModel = require('../models/userModel');
 const userRouter = express.Router();
+const bcrypt = require('bcrypt');
 
 userRouter.get('/', (req, res) => {
     res.send('Hi from Homepage');
 })
 
 app.post('/register', async(req, res) => {
+    const {password} = req.body;
     try {
-       const user = await UserModel.create(req.body); 
+       const newPass = bcrypt.hash(password, 10);
+       const user = await UserModel.create({...req.body, password : newPass}); 
        res.status(200).send({'msg' : 'User registered successfully', user});
     } catch (error) {
-        
+        res.status(400).send({'msg' : error.message});
     }
 })
 
