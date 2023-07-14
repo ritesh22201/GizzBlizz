09a4edff53c38677f8b40417a0d803d0cbd0700a
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-14 20:13:52 +0530

    code added

---
+++ fullStackCrud/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connection = require('./.configs/db');
+const userRouter = require('./routes/authRoutes');
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+app.use('/user', userRouter);
+
+
+app.listen(process.env.port, async() => {
+    try {
+        await connection;
+        console.log('Connected to the DB');
+    } catch (error) {
+        console.log(error.message);
+    }
+    console.log(`Server is running at port ${process.env.port}`);
+})
