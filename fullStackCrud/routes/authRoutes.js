2342eb1f57acf6a0448ea8ddab527d3e6e94a27e
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-14 21:22:36 +0530

    code added

--- fullStackCrud/routes/authRoutes.js
+++ fullStackCrud/routes/authRoutes.js
@@ -5,4 +5,12 @@ userRouter.get('/', (req, res) => {
     res.send('Hi from Homepage');
 })
 
+app.post('/register', async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
 module.exports = userRouter;
