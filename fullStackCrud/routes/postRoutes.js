e80bbc2b8ec908ace28b2115e377c2ca8cfd2f26
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-15 08:45:54 +0530

    code added

--- fullStackCrud/routes/postRoutes.js
+++ fullStackCrud/routes/postRoutes.js
@@ -6,9 +6,28 @@ const PostModel = require('../models/postModel');
 const auth = require('../middlewares/auth');
 
 postRouter.get('/', async (req, res) => {
+    const { q } = req.query;
     try {
-        const post = await PostModel.find();
+        if (q) {
+            const post = await PostModel.findOne({ title: { $regex: q, $options: 'i' } });
+            res.status(200).send(post);
+        }
+        else {
+            const post = await PostModel.find();
+            res.status(200).send(post);
+        }
+
+    } catch (error) {
+        res.status(400).send({ 'msg': error.message });
+    }
+})
+
+postRouter.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const post = await PostModel.findOne({_id : id});
         res.status(200).send(post);
+        
     } catch (error) {
         res.status(400).send({ 'msg': error.message });
     }
@@ -21,7 +40,7 @@ postRouter.post('/addPost', auth, async (req, res) => {
         if (existedPost) {
             res.status(400).send({ 'msg': 'Post already added in the DB' });
         }
-        else{
+        else {
             const post = await PostModel.create(req.body);
             await post.populate('userID');
             res.status(200).send({ 'msg': 'Post added Successfully', post });
