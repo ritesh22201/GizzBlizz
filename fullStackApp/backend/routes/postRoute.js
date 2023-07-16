08a13dbaf0cd5e3ec102e9c99583f3e4a4bb5965
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-16 19:01:15 +0530

    code added

--- fullStackApp/backend/routes/postRoute.js
+++ fullStackApp/backend/routes/postRoute.js
@@ -7,8 +7,9 @@ const auth = require('../middlewares/auth');
 const postRouter = express.Router();
 
 postRouter.get('/', auth, async(req, res) => {
+    const userID = req.body.userID;
     try {
-        const posts = await PostModel.find();
+        const posts = await PostModel.find({userID});
         res.status(200).send(posts);
     } catch (error) {
         res.status(400).send({'msg' : error.message});
@@ -27,6 +28,26 @@ postRouter.post('/add', auth, async(req, res) => {
             res.status(200).send({'msg' : 'Post added successfully', posts});
         }
        
+    } catch (error) {
+        res.status(400).send({'msg' : error.message});
+    }
+})
+
+postRouter.patch('/update/:id', auth, async(req, res) => {
+    const id = req.params.id;
+    try {
+        const updatedPost = await PostModel.findByIdAndUpdate({_id : id}, req.body, {new : true});
+        res.status(200).send({'msg' : 'Post updated successfully', updatedPost});
+    } catch (error) {
+        res.status(400).send({'msg' : error.message})
+    }
+})
+
+postRouter.delete('/delete/:id', auth, async(req, res) => {
+    const id = req.params.id;
+    try {
+        const deletedPost = await PostModel.findByIdAndDelete({_id : id});
+        res.status(200).send({'msg' : 'Post deleted successfully'});
     } catch (error) {
         res.status(400).send({'msg' : error.message});
     }
