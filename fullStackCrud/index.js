250523279504d339bad46f0c2929bf7129bc629c
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-16 09:39:04 +0530

    code added

--- fullStackCrud/index.js
+++ fullStackCrud/index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const connection = require('./.configs/db');
 const userRouter = require('./routes/authRoutes');
 const postRouter = require('./routes/postRoutes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const app = express();
 require('dotenv').config();
 app.use(express.json());
 app.use('/user', userRouter);
-app.use('/post', postRouter)
+app.use('/post', postRouter);
 
+const options = {
+    definition : {
+        openapi : "3.0.0",
+        info : {
+            title : 'Learning swagger for the first time',
+            version : '1.0.0'
+        },
+        server : {
+            url : 'http://localhost:8080'
+        }
+    },
+    apis : ["./routes/*.js"]
+}
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(process.env.port, async() => {
     try {
