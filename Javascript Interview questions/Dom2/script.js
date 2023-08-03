0298e7244883868e1fc37d2b8072b5b2d46879ee
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-03 18:48:21 +0530

    code added

--- Javascript Interview questions/Dom2/script.js
+++ Javascript Interview questions/Dom2/script.js
@@ -125,90 +125,136 @@ box2.textContent = 'Append inserts as last child" just after hr element.'
 
 // Select box 3 and prepend a new paragraph element with content "Prepend inserts as first child" just before hr element.
 
-
+let box3 = document.querySelector('.three');
+box3.textContent = 'Prepend inserts as first child" just before hr element.';
 
 // Change the border of box 4 to '1px solid black'
 
+let box4 = document.querySelector('.four');
+box4.style.border = '1px solid black';
 
+// Change the border radius of box 4 to 10px.
 
-// Change the border radius of box 5 to 10px.
-
+box4.style.borderRadius = '10px'
 
 // Change the text color of box 6 to black.
 
+let box6 = document.querySelector('.six');
+box6.style.color = 'black'
 
 // Change the font size of the para inside box 1 to 0.8rem.
 
+para.style.fontSize = '0.8rem'
 
 // Change the background of all the alternate boxes (1, 3, 5, ....) to aliceblue
 
-
+let box = document.querySelectorAll('.box');
+box.forEach((el, i) => {
+    if(i % 2 === 0){
+        el.style.backgroundColor = 'aliceblue';
+    }
+})
 
 // add a class named "awesome-box" to the box 6 using classList property of DOM element.
 
+let boxes6 = document.querySelector('.six');
+boxes6.classList.add('awesome-box')
 
 // Using the toggle classList property toggle the class `awesome-box` from box 2
 
+box2.classList.toggle('awesome-box');
 
 // Using the remove classList proeprty remove the class `awesome-box` from box 4
 
+box4.classList.remove('awesome-box');
+
 // Change the background of the body to bisque
 
+let body = document.querySelector('body'); 
+body.style.background = 'bisque'
 
 // Create a button and store it in a variable named 'btn'
 
+let btn = document.createElement('button');
 
 // textContent of the button should be 'Click Me'
 
+btn.textContent = 'Click Me';
 
 // Change the background of the btn to 'oldlace'
 
+btn.style.background = 'oldlace'
 
 // Change the font size of the btn to 1rem
 
+btn.style.fontSize = '1rem';
 
 // Change the border of the btn to '1px solid black'
 
+btn.style.border = '1px solid black'
 
 // Add the padding of '0.5rem 1rem' to btn
 
+btn.style.padding = '0.5rem 1rem';
 
 // Append the btn in box number 9
 
+let box9 = document.querySelector('.nine');
+box9.append(btn);
 
 // Create a img element with src value `https://images.unsplash.com/photo-1592500595497-d1f52a40b207?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80` and store in a variable named imgElm
 
+let imgElm = document.createElement('video');
+imgElm.src = 'https://v4.cdnpk.net/videvo_files/video/free/video0483/large_watermarked/_import_60d962f06b3ef8.86089157_FPpreview.mp4';
+imgElm.setAttribute('autoplay', true);
 
 // Select the box 7 using class seven
 
+let box7 = document.querySelector('.seven');
 
 // Remove all the elements form box seven
 
+while(box7.firstChild){
+    box7.removeChild(box7.firstChild);
+}
 
 // Append the imgElm to the box no 7
 
+box7.append(imgElm)
 
 // Change the width and height of the image to `100%`
 
+imgElm.style.width = '100%'
+imgElm.style.height = '100%'
 
 // Select the box 5 using class five
 
+let box5 = document.querySelector('.five');
 
 // Create an input element
 
+let input = document.createElement('input');
 
 // Change the placeholder property of the input element to "Enter you email!"
 
+input.setAttribute('placeholder', 'Enter you email!');
+
 // Append the input element to the box 5 you selected above
 
+box5.append(input);
 
 // Create two anchor (a) element with  the text of `AltCampus` and `Google`
 
-
+let alt = document.createElement('a');
+alt.textContent = 'AltCampus';
+let google = document.createElement('a');
+google.textContent = 'Google';
 
 // Change the href property of the anchor elements to `https://altcampus.school` and `https://google.com`
 
-
+alt.href = 'https://altcampus.school';
+google.href = 'https://google.com';
 
 // Append both the elements to box 5 you selected above.
 
+box5.append(alt, google);
