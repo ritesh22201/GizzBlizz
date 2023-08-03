197025d7e3c70e2bc1fd4020a6781ce950e08cd6
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-03 23:39:17 +0530

    code added

--- Javascript Interview questions/Random Color generator/index.js
+++ Javascript Interview questions/Random Color generator/index.js
@@ -4,9 +4,9 @@ let box1 = document.getElementById('box1');
 let box2 = document.getElementById('box2'); 
 
 function generateColor () {
-    let a = +Math.floor(Math.random()*256);
-    let b = +Math.floor(Math.random()*256);
-    let c = +Math.floor(Math.random()*256);
+    let a = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    let c = Math.floor(Math.random()*256);
     
     let rgbColor = `rgb(${a}, ${b}, ${c})`
     return rgbColor;
