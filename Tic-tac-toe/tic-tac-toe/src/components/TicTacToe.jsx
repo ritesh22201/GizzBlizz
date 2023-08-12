aed0fcd2761b1b4d1518c99659a59154a59ed66d
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-12 21:15:15 +0530

    code added

--- Tic-tac-toe/tic-tac-toe/src/components/TicTacToe.jsx
+++ Tic-tac-toe/tic-tac-toe/src/components/TicTacToe.jsx
@@ -1,11 +1,46 @@
-import { Box, Button, Flex, Grid } from '@chakra-ui/react'
+import { Box, Button, Flex, Grid, Text } from '@chakra-ui/react'
 import { CloseIcon } from '@chakra-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaO, FaXmark } from 'react-icons/fa6';
 import { GrPowerReset } from 'react-icons/gr';
 import Confetti from './Confetti';
+import '../styles/tictactoe.css';
 
 const TicTacToe = () => {
+    const [board, setBoard] = useState(Array(9).fill(''));
+    const [currentPlayerText, setCurrentPlayerText] = useState(true);
+
+    let outcomeArr = [
+       [0,1,2],
+       [3,4,5],
+       [6,7,8],
+       [0,3,6],
+       [1,4,7],
+       [2,5,8],
+       [0,4,8],
+       [2,4,6]
+    ]
+    
+    const handleInput = (e = '',n) => {
+        if (board[n] !== '') {
+            return;
+        }
+
+        board[n] = currentPlayerText ? 'X' : 'O';
+        setCurrentPlayerText(!currentPlayerText);
+        if(board[n] === 'X'){
+            e.target.style.color = '#37c6c0'
+        }
+        else{
+            e.target.style.color = '#f0b53d' 
+        }
+    }
+
+    const handleReset = () => {
+        let resetBoard = new Array(9).fill('');
+        setBoard(resetBoard);
+    }
+
     return (
         <Box bg={'#203138'} h={'100vh'}>
             <Box w={'46%'} m={'auto'} p={'20px'}>
@@ -14,19 +49,19 @@ const TicTacToe = () => {
                         <FaXmark fontSize={'32px'} color='#37c6c0' />
                         <FaO fontSize={'26px'} color='#f0b53d' />
                     </Flex>
-                    <Button onClick={() => <Confetti/>} _hover={'none'} bg={'#263b46'} color={'gray.300'}>X TURN</Button>
-                    <Button _hover={'none'} bg={'gray.300'}><GrPowerReset /></Button>
+                    <Button onClick={() => <Confetti />} _hover={'none'} bg={'#263b46'} color={'gray.300'}>{currentPlayerText ? 'X' : 'O'} TURN</Button>
+                    <Button _hover={'none'} bg={'gray.300'} onClick={handleReset}><GrPowerReset /></Button>
                 </Flex>
-                <Grid w={'70%'} m={'100px auto'} gap={'20px'} placeItems={'center'} gridTemplateColumns={'repeat(3, 1fr)'}>
-                    <Box>X</Box>
-                    <Box></Box>
-                    <Box>X</Box>
-                    <Box></Box>
-                    <Box>O</Box>
-                    <Box></Box>
-                    <Box>O</Box>
-                    <Box>X</Box>
-                    <Box>O</Box>
+                <Grid className='gridBox' w={'50%'} m={'100px auto'} gap={'20px'} placeItems={'center'} gridTemplateColumns={'repeat(3, 1fr)'}>
+                    <Box onClick={(e) => handleInput(e,0)}>{board[0]}</Box>
+                    <Box onClick={(e) => handleInput(e,1)}>{board[1]}</Box>
+                    <Box onClick={(e) => handleInput(e,2)}>{board[2]}</Box>
+                    <Box onClick={(e) => handleInput(e,3)}>{board[3]}</Box>
+                    <Box onClick={(e) => handleInput(e,4)}>{board[4]}</Box>
+                    <Box onClick={(e) => handleInput(e,5)}>{board[5]}</Box>
+                    <Box onClick={(e) => handleInput(e,6)}>{board[6]}</Box>
+                    <Box onClick={(e) => handleInput(e,7)}>{board[7]}</Box>
+                    <Box onClick={(e) => handleInput(e,8)}>{board[8]}</Box>
                 </Grid>
             </Box>
         </Box>
