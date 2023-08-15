f9acb1a8a430185db3ca8c2ac7bba78ba6937148
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-15 22:24:18 +0530

    code added

--- three.js/Project1/script.js
+++ three.js/Project1/script.js
@@ -5,24 +5,47 @@ const scene = new THREE.Scene();
 
 // Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color : 0xf0000});
+const material = new THREE.MeshBasicMaterial({ color: 0xf0000 });
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 //Sized 
 
 const sizes = {
-    width : 800,
-    height : 1000
+    width: 800,
+    height: 1000
 }
 
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 scene.add(camera);
 
 //Render 
 
 const renderer = new THREE.WebGLRenderer({
-    canvas : canvas
+    canvas: canvas
 })
+
+renderer.setSize(sizes.width, sizes.height);
+
+
+
+
+
+
+
+
+
+
+function abc(a) {
+    return function foo(b) {
+        return function fun(c) {
+            return function greet(d) {
+                return a + b + c + d;
+            };
+        }
+    }
+}
+
+console.dir(abc(4)(3)(2));
