ed9cc436ff4389b2fb693fa5b700c8ae245a5acc
ritesh22201 <riteshgoswami22201@gmail.com>
2023-09-04 23:30:11 +0530

    code added

--- Oops/index.js
+++ Oops/index.js
@@ -23,4 +23,30 @@ function createUser(name, score = 0) {
 
 
 const user1 = createUser('Arya', 10);
-console.log(user1.changeName('John'));
+// console.log(user1.changeName('John'));
+
+
+class CreateUser {
+    constructor(name, score = 0){
+        this.name = name;
+        this.score = score;
+    }
+
+    increaseScore (val) {
+        this.score += val
+        return this.score
+    }
+    
+    decreaseScore(value) {
+        this.score -= value
+        return this.score
+    }
+    
+    changeName(val) {
+        this.name = val;
+        return this.name;
+    }
+}
+
+const user2 = new CreateUser('John Snow', 34);
+console.log(user2.increaseScore(10));
