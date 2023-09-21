f432512daf2c4854e00c29f26a9c381b1b19175e
ritesh22201 <riteshgoswami22201@gmail.com>
2023-09-21 23:08:03 +0530

    code added

--- react-dynamic-data/src/Components/Cities.jsx
+++ react-dynamic-data/src/Components/Cities.jsx
@@ -2,13 +2,40 @@ import React, { useState } from 'react'
 
 const Cities = () => {
     const [isOpen, setIsOpen] = useState(null);
+
     const data = [
         { name: "India", value: "IN", cities: ["Delhi", "Mumbai"] },
         { name: "Australia", value: "AUS", cities: ["Sydney", "Canberra"] },
         { name: "USA", value: "US", cities: ["New York", "Los Angeles"] }
     ];
+
+    const handleChange = (e) => {
+        let index = data.findIndex((ele) => ele.value === e.target.value);
+        setIsOpen(index);
+    };
+
     return (
-        <div>Cities</div>
+        <>
+            <select onChange={handleChange}>
+                <option value="">Select Country</option>
+                {data.map((el, i) => {
+                    return (
+                        <option key={i} value={el.value}>
+                            {el.name}
+                        </option>
+                    );
+                })}
+            </select>
+            <select>
+                {data[isOpen]?.cities.map((el, i) => {
+                    return (
+                        <option key={i} value={el}>
+                            {el}
+                        </option>
+                    );
+                })}
+            </select>
+        </>
     )
 }
 
