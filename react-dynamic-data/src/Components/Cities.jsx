ab552bf3a90f479eaebd15eb3ade20443f66c6be
ritesh22201 <riteshgoswami22201@gmail.com>
2023-09-21 23:07:00 +0530

    code added

--- react-dynamic-data/src/Components/Cities.jsx
+++ react-dynamic-data/src/Components/Cities.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cities = () => {
-  return (
-    <div>Cities</div>
-  )
+    const [isOpen, setIsOpen] = useState(null);
+    const data = [
+        { name: "India", value: "IN", cities: ["Delhi", "Mumbai"] },
+        { name: "Australia", value: "AUS", cities: ["Sydney", "Canberra"] },
+        { name: "USA", value: "US", cities: ["New York", "Los Angeles"] }
+    ];
+    return (
+        <div>Cities</div>
+    )
 }
 
 export default Cities;
