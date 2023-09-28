76c444c9c8163425bdc7d82c5bf258bc83534d52
ritesh22201 <riteshgoswami22201@gmail.com>
2023-09-28 23:04:49 +0530

    code added

---
+++ New folder/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cities from './Components/Cities';
+
+function App() {
+  return (
+    <div className="App">
+      <Cities/>
+    </div>
+  );
+}
+
+export default App;
